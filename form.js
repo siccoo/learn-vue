6d5d3948012beaa4781a459b3df5a59cb8883b41
Michael Chilaka <chilakamichael001@gmail.com>
2019-06-05 13:10:44 +0100

    form.html, form.css, form.js, Installation of vue dependencies

---
+++ form.js
@@ -0,0 +1,18 @@
+
+var App = new Vue ({
+    el: '#app',
+    data: {
+        name: '',
+        subject: '',
+        termsAndConditions: 'false',
+        yesOrNo: ''
+    },
+    methods: {
+        submit(){
+            console.log('name', this.name);
+            console.log('subject', this.subject);
+            console.log('termsAndConditions', this.termsAndConditions);
+            console.log('yesOrNo', this.yesOrNo);
+        }
+    }
+})
