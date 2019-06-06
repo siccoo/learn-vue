95024d28410ce8423b0be6ae3ec49fae70510875
Michael Chilaka <chilakamichael001@gmail.com>
2019-06-06 16:19:20 +0100

    Understanding Watchers and Computed Properties

--- metric.js
+++ metric.js
@@ -6,20 +6,43 @@
 //         }
 //     }
 
+// new Vue({
+//     el: '#app',
+//     data: {
+//         km: 0,
+//         m: 0
+//     }, 
+//     watch: {
+//         km() {
+            // whenever km changes, this function runs
+        //     this.m = this.km ? this.km * 1000 : 0;
+        // },
+        // m() {
+            // whenever m changes, this function runs
+//             this.km = this.m ? this.m / 1000 : 0;
+//         }
+//     }
+// });
+
+// WATCHERS VS. COMPUTED PROPERTIES
+// We could build the same application without the use of watchers and
+// instead use computed properties. We know computed properties allow us
+// to compute a new property from an existing data property.
+
 new Vue({
     el: '#app',
     data: {
         km: 0,
         m: 0
-    }, 
-    watch: {
-        km() {
-            // whenever km changes, this function runs
-            this.m = this.km ? this.km * 1000 : 0;
-        },
-        m() {
-            // whenever m changes, this function runs
-            this.km = this.m ? this.m / 1000 : 0;
+    },
+    computed: {
+        m: {
+            get() {
+                return this.km * 1000;
+            },
+            set(newValue) {
+                return this.km = newValue/1000;
+            }
         }
     }
-});
+})
