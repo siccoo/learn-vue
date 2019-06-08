37d5058481dbdd3a7638a99d12924b7f8221de42
Michael Chilaka <chilakamichael001@gmail.com>
2019-06-08 15:14:09 +0100

    Vue Component and Template

--- twitter.js
+++ twitter.js
@@ -30,3 +30,45 @@ new Vue({
         tweets
     }
 });
+
+Vue.component('tweet-component', {
+    template: `
+    <div class="tweet">
+    <div class="box">
+        <article class="media">
+            <div class="media-left">
+                <figure class="image is-64x64">
+                    <img :src="tweet.img">
+                </figure>
+            </div>
+            <div class="media-content">
+                <div class="content">
+                    <p>
+                        <strong>{{tweet.name}}</strong>
+                        <small>{{tweet.handle}}</small>
+                        <br>
+                        {{tweet.tweet}}
+                    </p>
+                </div>
+                <div class="level-left">
+                    <a class="level-item">
+                        <span class="icon is-small">
+                            <i class="fas fa-heart"></i>
+                        </span>
+                        <span class="likes">
+                            {{tweet.likes}}
+                        </span>
+                    </a>
+                </div>
+            </div>
+        </article>
+    </div>
+    <div class="control has-icons-left">
+        <input class="input is-small" placeholder="Tweet your reply..." />
+        <span class="icon is-small is-left">
+            <i class="fas fa-envelope"></i>
+        </span>
+    </div>
+    </div> 
+    `
+})
