d39c1202004d1b85b1d557edc337b2134e7b06f4
Michael Chilaka <chilakamichael001@gmail.com>
2019-06-10 18:50:37 +0100

    localComponent | Root template

--- global.js
+++ global.js
@@ -25,3 +25,65 @@ new Vue({
         'local-component': localComponent
     }
 });
+
+
+// For components, data should always be a function that returns an object.
+let componentObject = {
+    template: '<p>{{message}}</p>',
+    data() {
+        return {
+            message: 'Greetings!'
+        }
+    }
+}
+
+let counterOne = {
+    template: `
+        <button class="component-button" @click="counter++">
+            You clicked me {{counter}} times
+        </button>
+    `,
+    data() {
+        return {
+            counter: 0
+        }
+    }
+}
+
+let counterTwo = {
+    template: `
+        <button class="component-button" @click="counter++">
+            You clicked me {{counter}} times
+        </button>
+    `,
+    data() {
+        return {
+            counter: 0
+        }
+    }
+}
+
+let counterThree = {
+    template: `
+        <button class="component-button" @click="counter++">
+            You clicked me {{counter}} times
+        </button>
+    `,
+    data() {
+        return {
+            counter: 0
+        }
+    }
+}
+
+new Vue({
+    el: '#app',
+    data: {
+        message: 'Dora and Moya are mates'
+    },
+    components: {
+        'counter-one': counterOne,
+        'counter-two': counterTwo,
+        'counter-three': counterThree
+    }
+});
