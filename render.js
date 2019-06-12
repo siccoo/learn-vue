f14c6f174eb7f5343338e2397bbfce7ae2a3cd8b
Michael Chilaka <chilakamichael001@gmail.com>
2019-06-12 12:23:22 +0100

    renderComponent

--- render.js
+++ render.js
@@ -1,38 +1,38 @@
 
-new Vue({
-    el: '#app',
-    data: {
-        message: 'Eliora!!!'
-    }
-})
-
-let renderComponent = {
-    render(h) {
-        return h(
-            'div',{
-                'attr': {
-                    class: 'render-card'
-                }, 
-            }, [
-                h('header', {
-                    'attr': {
-                        class: 'card-header card-header-title',
-                    }
-                }, this.message)
-            ]
-        );
-    },
-    props: ['message']
-};
+// new Vue({
+//     el: '#app',
+//     data: {
+//         message: 'Eliora!!!'
+//     }
+// })
 
 // let renderComponent = {
 //     render(h) {
-//         return(
-//             <div class="render-card">
-//                 <header class="card-header card-header-title">
-//                     {this.message}
-//                 </header>
-//             </div>
-//         )
-//     }
-// }
+//         return h(
+//             'div',{
+//                 'attr': {
+//                     class: 'render-card'
+//                 }, 
+//             }, [
+//                 h('header', {
+//                     'attr': {
+//                         class: 'card-header card-header-title',
+//                     }
+//                 }, this.message)
+//             ]
+//         );
+//     },
+//     props: ['message']
+// };
+
+let renderComponent = {
+    render(h) {
+        return(
+            <div class="render-card">
+                <header class="card-header card-header-title">
+                    {this.message}
+                </header>
+            </div>
+        )
+    }
+}
