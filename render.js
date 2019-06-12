bfbf97a4311958e5b8a8712540df15a30906460b
Michael Chilaka <chilakamichael001@gmail.com>
2019-06-12 16:45:56 +0100

    createElement

--- render.js
+++ render.js
@@ -25,14 +25,5 @@
 //     props: ['message']
 // };
 
-let renderComponent = {
-    render(h) {
-        return(
-            <div class="render-card">
-                <header class="card-header card-header-title">
-                    {this.message}
-                </header>
-            </div>
-        )
-    }
-}
+// 
+
