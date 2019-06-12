ae5b7ed3a37af3221adb2233c89d07252f4b2b09
Michael Chilaka <chilakamichael001@gmail.com>
2019-06-12 11:57:50 +0100

    Render func & JSX

--- render.js
+++ render.js
@@ -1,14 +1,38 @@
+
+new Vue({
+    el: '#app',
+    data: {
+        message: 'Eliora!!!'
+    }
+})
+
 let renderComponent = {
-    render(createElement) {
-        return createElement(
+    render(h) {
+        return h(
             'div',{
                 'attr': {
                     class: 'render-card'
                 }, 
             }, [
-                createElement('header')
+                h('header', {
+                    'attr': {
+                        class: 'card-header card-header-title',
+                    }
+                }, this.message)
             ]
         );
     },
     props: ['message']
 };
+
+// let renderComponent = {
+//     render(h) {
+//         return(
+//             <div class="render-card">
+//                 <header class="card-header card-header-title">
+//                     {this.message}
+//                 </header>
+//             </div>
+//         )
+//     }
+// }
